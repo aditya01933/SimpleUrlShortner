56635ad17c707a040f51e7056c704543fe1da5ba
aditya01933 <adityatiwari01933@gmail.com>
2016-08-27 13:04:09 +0530

    TDD test cases implemented (failing)

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -2,14 +2,33 @@ require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
 	describe '#POST .create' do
-		it 'should create a short url'
-		it 'should return 200 status'
+		it 'should create a short url' do
+			count = Link.count
+			@response = response
+			post :create, link: {given_url: "youtube.com"}
+			expect(Link.count).to eq(count+1) 
+		end
+		it 'should return 200 status' do
+			expect(@response.status).to be 200
+		end
 	end
 	describe '#GET show' do
-		it 'should redirect to given url'
-		it 'should increase link count'
+		it 'should redirect to given url' do
+			link = Link.first
+			get :show, id: link.slug  
+			expect(response).to redirect_to(link.given_url)
+		end
+		it 'should increase visit count' do
+			link = Link.first
+			get :show, id: link.slug  
+			expect(Link.first.clicks).to eq(link.clicks + 1)
+		end
 	end
 	describe '#GET index' do
-		it 'should be able to list all links according to pagination'
+		it 'should be able to list all links according to pagination' do
+			links = Link.all
+			get :index
+			expect(response).to render_template :index
+		end
 	end
 end
