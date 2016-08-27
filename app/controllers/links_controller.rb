a90e8b21804b03fbb99918992e34591faa1b9a3b
aditya01933 <adityatiwari01933@gmail.com>
2016-08-27 16:56:24 +0530

    url shortner create logic

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,9 +2,17 @@ class LinksController < ApplicationController
 
 	def index
 		@link = Link.new
-		@links = Link.all
+		@links = Link.all.order('created_at DESC')
 	end
-	def create
-		
+	def create		
+		@link = Link.new(link_params)
+		unless @link.save
+			render 'layouts/error'
+		end
 	end
+
+	private
+	 def link_params
+      params.require(:link).permit(:title, :given_url)
+    end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,13 +1,24 @@
 class Link < ApplicationRecord
+	validates :slug, presence: true
 
-	def generate_slug!
-    self.slug = self.id.to_s(36)
-    self.save!
-  end
+	before_validation :generate_slug
 
   def increment_clicks!
   	self.clicks += 1
   	self.save!
   end
+
+  def short_url
+  	"http://adi.tw/" + self.slug 
+  end
+
+  protected
+  
+  	def generate_slug
+	    self.slug = loop do
+	      random_token = SecureRandom.hex(3)
+	      break random_token unless self.class.exists?(slug: random_token)
+	    end
+  	end
   
 end
