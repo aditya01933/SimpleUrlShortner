7675c80c5839c0f71f52f4e387a91196210253fe
aditya01933 <adityatiwari01933@gmail.com>
2016-08-27 13:18:30 +0530

    TDD for model testing

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-	it 'should be able to generate slug'
-	it 'should be able to track visits'	
+	describe '.generate_slug' do
+		it 'should be able to generate slug' do
+			slug = Link.new.generate_slug
+			expect(slug).not_to be nil
+		end
+	end
+	describe '.increment_clicks! do'
+		it 'should be able to increase clicks'	do
+			link = Link.create(given_url: "http://www.yahoo.com")
+			old_count = link.clicks
+			new_click_count = link.increment_clicks!
+			expect(link.clicks).to eq(old_count+1)
+		end
+	end
 end
